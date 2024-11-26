58a4f3ff70150a69c6b5b5d775ca809ffaab8ea7
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-26 16:07:40 +0100

    Configuration de la fonction first_connection_message_view afin d'afficher un panneau d'information en utilisant la fonctionnalitée rich

--- views/firststartview.py
+++ views/firststartview.py
@@ -6,6 +6,16 @@ console = Console()
 
 class FirstStartView:
     def first_connection_message_view(self):
-        pass
-    
+        cadre = Panel(
+            "\n[red blink]Démarrage de l'application...[/red blink]\n\n"
+            "[bold wihte]Vous devez saisir l'identifiant et le mot de passe d'un compte [/bold wihte] \n"
+            "[bold yellow]administrateur MySQL[/bold yellow] [bold wihte]afin de procéder à la création[/bold wihte] "
+            "[bold wihte]de votre base de données.[/bold wihte]",  # Texte stylisé avec Rich
+            title="[blue] Epic Events[/blue]",  # Titre du cadre (optionnel)
+            expand=True,  # Le cadre prend toute la largeur du terminal
+            border_style="red",  # Style de la bordure
+        )
+
+        console.print(cadre, justify="left")
+
 
