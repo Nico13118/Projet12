57e5c4ae06e83c6929c47c19254575e3da880588
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-26 15:39:54 +0100

    Importation de la bibliothèque rich. Préconfiguration de la classe FirstStartView et de la fonction first_connection_message_view

---
+++ views/firststartview.py
@@ -0,0 +1,11 @@
+from rich.panel import Panel
+from rich.console import Console
+from rich.prompt import Prompt
+console = Console()
+
+
+class FirstStartView:
+    def first_connection_message_view(self):
+        pass
+    
+
