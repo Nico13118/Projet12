e97624a2cdbfdf0a0d0c1df7a650eb3f74ff05c9
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-26 17:29:36 +0100

    Ajout du fichier usercontroller.py, importation de la bibliothèque sqlalchemy et urllib, configuration de la classe UserController et de la fonction test_username_password_controller qui aura pour utilité de tester l'identifiant et mot de passe de l'utilisateur.

---
+++ controllers/usercontroller.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from urllib.parse import quote
+
+
+class UserController:
+    def __init__(self, controller):
+        self.controller = controller
+
+    def test_username_password_controller(self, username, password1):
+        password2 = quote(password1)
+        try:
+            engine = create_engine(f'mysql+pymysql://{username}:{password2}@localhost')
+            with engine.connect() as connection:
+                return True
+        except ValueError:
+            return False
+        
