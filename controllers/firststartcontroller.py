618fec44f8f488128254a8fde365d74dbf90904a
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-26 19:19:10 +0100

    Petite modification dans la fonction init au niveau des paramètres. Configuration de la fonction first_start_controller qui appel une fonction de le vue et une fonction interne. Configuration de la fonction username_password_controller qui a pour fonction d'appeler la vue pour demander l'identifiant et mot de passe de l'utilisateur puis va appeler une autre fonction d'un fichier controlleur pour tester les identifiants. En cas d'erreur une fonction est appelé dans la vu pour signaler l'utilisateur et une boucle est mis en place afin que l'utilisateur puise resaisir ses identifiants.

--- controllers/firststartcontroller.py
+++ controllers/firststartcontroller.py
@@ -1,7 +1,21 @@
 class FirstStartController:
-    def __init__(self, controller):
-        self.controller = controller
+    def __init__(self, first_start_view, usercontroller):
+        self.first_start_v = first_start_view
+        self.user_c = usercontroller
 
     def first_start_controller(self):
-        pass
+        self.first_start_v.first_connection_message_view()
+        list_username_password = self.username_password_controller()
+        
+
+    def username_password_controller(self):
+        connexion_test = False
+        while not connexion_test:
+            result_username = self.first_start_v.get_username_view()
+            result_password = self.first_start_v.get_password_view()
+            connexion_test = self.user_c.test_username_password_controller(result_username, result_password)
+            if connexion_test:
+                return [result_username, result_password]
+            else:
+                self.first_start_v.error_username_password_view()
 
