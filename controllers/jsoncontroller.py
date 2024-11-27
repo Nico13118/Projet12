4db1d113488b8bc325b46bf2354cf4e373ce71ea
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-27 15:37:34 +0100

    Ajout de la fonction create_json_info_file_controller, fonction qui permet de d'envoyer les informations dans le modèle afin qu'il soit retourner sous la forme d'un dictionnaire puis procède à la création et à l'enregistrement des données dans le fichier json.

--- controllers/jsoncontroller.py
+++ controllers/jsoncontroller.py
@@ -1,4 +1,7 @@
 import os
+import json
+import time
+from models.jsonmodel import JsonModel
 
 project_root = os.getcwd()
 
@@ -11,3 +14,10 @@ class JsonController:
         result = os.path.exists(f'{project_root}/info.json')
         return result
 
+    def create_json_info_file_controller(self, database_name):
+        date_time = time.strftime('%d/%m/%Y - %H:%M')
+        jsonmodel = JsonModel(database_name, date_time)
+        result_json = jsonmodel.create_json_model()
+
+        with open(f'{project_root}/info.json', 'w') as file_json:
+            json.dump([result_json], file_json)
