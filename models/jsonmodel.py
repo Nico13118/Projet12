de61d77f28a1c2f72c489e779dfaf5e137af7ebd
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-27 15:00:51 +0100

    Ajout de la classe JsonModel, configuration de la fonction init avec en paramètre le nom de la database et la date de création. Configuration de la fonction create_json_model qui permet de créer et de retourner les informations sous la forme d'un dictionnaire

--- models/jsonmodel.py
+++ models/jsonmodel.py
@@ -1,2 +1,13 @@
+class JsonModel:
+    def __init__(self, database_name, created_date):
+        self.database_name = database_name
+        self.created_date = created_date
+
+    def create_json_model(self):
+        info_database = {
+            "name": self.database_name,
+            "created_date": self.created_date
+        }
+        return info_database
 
 
