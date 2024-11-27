c713f23fb1b2d1683b5b57d78dce59886e305f40
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-11-27 16:37:27 +0100

    Suppression de la méthode init car inutilisé.

--- controllers/jsoncontroller.py
+++ controllers/jsoncontroller.py
@@ -7,9 +7,6 @@ project_root = os.getcwd()
 
 
 class JsonController:
-    def __init__(self, controller):
-        self.controller = controller
-
     def search_json_file_controller(self):
         result = os.path.exists(f'{project_root}/info.json')
         return result
