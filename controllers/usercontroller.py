50f22f64459382664bba026540ac857cff460b5d
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-02 17:19:04 +0100

    Ajout d'une constante ROLE. Configuration de la fonction start_create_user_controller qui permet de gérer les autres fonction pour la création et l'enregistrement d'un collaborateur. Configuration de la fonction get_role_id_controller qui permet de retourner l'id selon le role de l'utilisateur. Configuration de la fonction search_is_alpha_controller qui permet vérifier qu'une saisie utilisateur ne contient que des lettres. Ajout de la fonction get_name_controller, get_first_name_controller, get_email_controller, get_username_controller, get_password_controller et get_role_controller, ces fonctions sont prévues pour récupérer et vérifie la saisie utilisateur et la retourne si OK.

--- controllers/usercontroller.py
+++ controllers/usercontroller.py
@@ -1,17 +1,118 @@
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 from urllib.parse import quote
+import re
+
+ROLE = ["COM", "GES", "SUP"]
 
 
 class UserController:
-    def __init__(self, controller):
-        self.controller = controller
+    def __init__(self, userview, tablecontroller, databasecontroller):
+        self.user_v = userview
+        self.table_c = tablecontroller
+        self.database_c = databasecontroller
 
-    def test_username_password_controller(self, username, password1):
-        password2 = quote(password1)
+    def test_username_password_controller(self, username_admin, password_admin):
+        pass_admin = quote(password_admin)
         try:
-            engine = create_engine(f'mysql+pymysql://{username}:{password2}@localhost')
+            engine = create_engine(f'mysql+pymysql://{username_admin}:{pass_admin}@localhost')
             with engine.connect() as connection:
                 return True
         except OperationalError:
             return False
+
+    def start_create_user_controller(self, username_admin, password_admin):
+        info_name = self.get_name_controller()
+        info_first_name = self.get_first_name_controller()
+        info_email = self.get_email_controller()
+        info_username = self.get_username_controller(info_name, info_first_name)
+        info_password = self.get_password_controller()
+        info_role = self.get_role_controller()
+
+        role_id = self.get_role_id_controller(info_role)  # Récupération de role_id
+
+        self.table_c.save_collaborator_in_table_controller(username_admin, password_admin,
+                                                           info_name, info_first_name,
+                                                           info_email, info_username,
+                                                           info_password, role_id)
+        if role_id == 1:
+            self.database_c.save_collaborator_com_in_mysql_controller(username_admin, password_admin,
+                                                                      info_username, info_password)
+        if role_id == 2:
+            pass
+        if role_id == 3:
+            pass
+
+    def get_role_id_controller(self, info_role):
+        role = ""
+        if info_role == "COM":
+            role = 1
+        elif info_role == "GES":
+            role = 2
+        elif info_role == "SUP":
+            role = 3
+        return role
+
+    def search_is_alpha_controller(self, user_input1):
+        user_input2 = re.sub(r"\s", "", user_input1)
+        x = user_input2.isalpha()
+        return x
+
+    def get_name_controller(self):
+        while True:
+            user_input = self.user_v.get_name_view()  # Récupère la saisie de l'utilisateur
+            if user_input:  # Vérifie que la saisie n'est pas vide
+                if self.search_is_alpha_controller(user_input):  # Vérifie que la saisie contient que des lettres
+                    break
+                else:
+                    self.user_v.error_message_field_contains_number()
+            else:
+                self.user_v.error_message_empty_field_view()
+        return user_input
+
+    def get_first_name_controller(self):
+        while True:
+            user_input = self.user_v.get_first_name_view()  # Récupère la saisie de l'utilisateur
+            if user_input:  # Vérifie que la saisie n'est pas vide
+                if self.search_is_alpha_controller(user_input):  # Vérifie que la saisie contient que des lettres
+                    break
+            else:
+                self.user_v.display_message_error_field_view()
+        return user_input
+
+    def get_email_controller(self):
+        while True:
+            user_input = self.user_v.get_email_view()  # Récupère la saisie de l'utilisateur
+            if user_input:  # Vérifie que la saisie n'est pas vide
+                break
+            else:
+                self.user_v.display_message_error_field_view()
+        return user_input
+
+    def get_username_controller(self, name, first_name):
+        lower_name = name.lower()
+        lower_first_name = first_name()
+        username = f"{lower_name}_{lower_first_name}"
+        return username
+
+    def get_password_controller(self):
+        while True:
+            user_input = self.user_v.get_password_view()  # Récupère la saisie de l'utilisateur
+            if user_input:
+                break
+            else:
+                self.user_v.display_message_error_field_view()
+        return user_input
+
+    def get_role_controller(self):
+        while True:
+            user_input = self.user_v.get_role_view()  # Récupère la saisie de l'utilisateur
+            if user_input:
+                user_input = user_input.upper()
+                if user_input in ROLE:
+                    break
+                else:
+                    self.user_v.display_message_error_choices_view()
+            else:
+                self.user_v.display_message_error_field_view()
+        return user_input
