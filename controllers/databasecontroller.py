1bc188693e3b17a7f4919831878b95f6135044e8
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-03 07:42:43 +0100

    Configuration de la fonction save_collaborator_com_in_my_sql_controller, fonction qui permet de créer un utilisateur mysql et de lui ajouter ses droits selon son role.

--- controllers/databasecontroller.py
+++ controllers/databasecontroller.py
@@ -3,8 +3,32 @@ from urllib.parse import quote
 
 
 class DatabaseController:
+    def __init__(self, jsoncontroller):
+        self.json_c = jsoncontroller
+
     def create_databases_controller(self, username, password1, database_name):
         password2 = quote(password1)
         engine = create_engine(f'mysql+pymysql://{username}:{password2}@localhost')
         with engine.connect() as connection:
             connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {database_name}"))
+
+    def save_collaborator_com_in_mysql_controller(self, username_admin, password_admin,
+                                                  info_username, info_password):
+        """
+        Le commercial doit pouvoir :
+        Customer : Créer ou mettre à jour des clients. >> Table customer SELECT, INSERT, UPDATE
+        Contract : Modifier / Mettre à jour les contrats >> Table contract SELECT, INSERT, UPDATE
+        Event :  Créer un évènement INSERT,
+
+        """
+        pass_admin = quote(password_admin)
+        database_name = self.json_c.get_database_name_in_json_file()
+        engine = create_engine(f'mysql+pymysql://{username_admin}:{pass_admin}@localhost/{database_name}')
+        with engine.connect() as connection:
+            connection.execute(text(f"CREATE USER {info_username}@localhost IDENTIFIED BY {info_password}"))
+            connection.execute(text(f"GRANT SELECT, INSERT, UPDATE ON {database_name}.customer "
+                                    f"TO {info_username}@localhost"))
+            connection.execute(text(f"GRANT SELECT, INSERT, UPDATE ON {database_name}.contract "
+                                    f"TO {info_username}@localhost"))
+            connection.execute(text(f"GRANT INSERT ON {database_name}.event TO {info_username}@localhost"))
+            connection.execute(text("FLUSH PRIVILEGES"))
