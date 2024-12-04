047c2973fd526fe9d8034c8191dfa78dc82062cd
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-04 17:09:20 +0100

    Configuration de la fonction display_error_message_view, fonction qui permet d'afficher un message d'erreur à l'utilisateur suite à un mauvais choix du menu. Configuration de la fonction get_user_input qui permet de demander à l'utilisateur de saisir une valeur selon le menu. Configuration de la fonction clear_terminal_view, fonction qui permet de réinitialiser l'affichage du terminal. Configuration de la fonction display_message_create_new_collaborator, fonction qui permet d'afficher un panneau d'information concernant la saisie des information lors de la création d'un nouveau collaborateur.

--- views/menuview.py
+++ views/menuview.py
@@ -1,7 +1,8 @@
 from rich.prompt import Prompt
 from rich.panel import Panel
 from rich.console import Console
-
+from rich.text import Text
+import os
 console = Console()
 
 
@@ -9,11 +10,11 @@ class MenuView:
     def display_menu_gestion_view(self):
         cadre = Panel(
             "\n[bold green]1) Enregistrer un nouveau collaborateur[/bold green]\n"
-            "\n[bold green]2) Afficher la liste des collaborateurs[/bold green]\n"  # Associers au commercial  
+            "\n[bold green]2) Afficher la liste des collaborateurs[/bold green]\n"    
             "\n[bold green]3) Créer un contrat[/bold green]\n"
             "\n[bold green]4) Afficher la liste des contrats[/bold green]\n"
             "\n[bold green]5) Afficher la liste des évènements (non associé)[/bold green]\n"
-            "\n[bold green]6) Afficher tous les clients (Lecture seule)[/bold green]\n"  # Associers au commercial
+            "\n[bold green]6) Afficher tous les clients (Lecture seule)[/bold green]\n"  
             "\n[bold green]7) Afficher tous les contrats (Lecture seule)[/bold green]\n"
             "\n[bold green]8) Afficher tous les évènements (Lecture seule)[/bold green]\n"
             "\n[bold green]9) Quitter l'application [/bold green]\n",
@@ -22,13 +23,11 @@ class MenuView:
             border_style="green",  # Style de la bordure
         )
         console.print(cadre, justify="left")
-        user_input = Prompt.ask("[bold green]Faite votre choix [/bold green]")
-        return user_input
 
     def display_menu_commercial_view(self):
         cadre = Panel(
             "\n[bold green]1) Enregistrer un nouveau client[/bold green]\n"
-            "\n[bold green]2) Afficher ma liste de clients[/bold green]\n"  # Associers au commercial  
+            "\n[bold green]2) Afficher ma liste de clients[/bold green]\n"  
             "\n[bold green]3) Afficher les contrats signé[/bold green]\n"  
             "\n[bold green]4) Afficher les contrats non signé[/bold green]\n"
             "\n[bold green]5) Afficher les contrats non réglé[/bold green]\n"
@@ -41,5 +40,24 @@ class MenuView:
             border_style="green",  # Style de la bordure
         )
         console.print(cadre, justify="left")
+
+    def display_error_message_view(self):
+        text = Text("Choix invalide, veuillez recommencer.")
+        text.stylize("bold red")
+        console.print(text)
+
+    def get_user_input_view(self):
         user_input = Prompt.ask("[bold green]Faite votre choix [/bold green]")
-        return user_input
+        return int(user_input)
+
+    def clear_terminal_view(self):
+        os.system("cls")
+
+    def display_message_create_new_collaborator(self):
+        cadre = Panel(
+            "\n[bold green]Veuillez saisir les informations du nouveau collaborateur.[/bold green]\n",
+            title="[blue] Epic Events Enregistrement[/blue]",
+            expand=True,
+            border_style="green",
+        )
+        console.print(cadre, justify="left")
