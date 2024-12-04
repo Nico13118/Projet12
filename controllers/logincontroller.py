67b457ab27a4eba25bfb8974ecc1611120f81133
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-04 16:21:18 +0100

    Configuration de la fonction start_authentication_controller, fonction qui permet de récupérer un username et password d'un utilisateur et fait appel à une fonction pour vérifier son existance dans la base MySQL.

--- controllers/logincontroller.py
+++ controllers/logincontroller.py
@@ -1,5 +1,23 @@
 class LoginController:
-    def __init__(self, controller):
-        self.controller = controller
-        
-        pass
+    def __init__(self, loginview, firststartview, usercontroller):
+        self.user_c = usercontroller
+        self.login_v = loginview
+        self.first_start_v = firststartview
+
+    def start_authentication_controller(self):
+        """
+        Fonction qui gère la récupération d'un username et password d'un utilisateur et fait appel à une
+        fonction pour vérifier que l'utilisateur existe dans la base MySQL.
+
+        :return: username , password
+        """
+        self.login_v.display_message_info_authentication()
+        while True:
+            username = self.login_v.get_username_view()
+            password = self.login_v.get_password_view()
+            control_authentication = self.user_c.test_username_password_controller(username, password)
+            if not control_authentication:
+                self.first_start_v.error_username_password_view()
+            else:
+                break
+        return username, password
