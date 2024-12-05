8e15da97edc76090f208d9067c8818bd2688f7fa
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-05 14:33:46 +0100

    Modification des couleurs sur l'ensemble des fonctions

--- views/menuview.py
+++ views/menuview.py
@@ -9,35 +9,35 @@ console = Console()
 class MenuView:
     def display_menu_gestion_view(self):
         cadre = Panel(
-            "\n[bold green]1) Enregistrer un nouveau collaborateur[/bold green]\n"
-            "\n[bold green]2) Afficher la liste des collaborateurs[/bold green]\n"    
-            "\n[bold green]3) Créer un contrat[/bold green]\n"
-            "\n[bold green]4) Afficher la liste des contrats[/bold green]\n"
-            "\n[bold green]5) Afficher la liste des évènements (non associé)[/bold green]\n"
-            "\n[bold green]6) Afficher tous les clients (Lecture seule)[/bold green]\n"  
-            "\n[bold green]7) Afficher tous les contrats (Lecture seule)[/bold green]\n"
-            "\n[bold green]8) Afficher tous les évènements (Lecture seule)[/bold green]\n"
-            "\n[bold green]9) Quitter l'application [/bold green]\n",
-            title="[blue] Epic Events Menu Gestion[/blue]",  # Titre du cadre (optionnel)
+            "\n[bright_cyan]1) Enregistrer un nouveau collaborateur[/bright_cyan]\n"
+            "\n[bright_cyan]2) Afficher la liste des collaborateurs[/bright_cyan]\n"    
+            "\n[bright_cyan]3) Créer un contrat[/bright_cyan]\n"
+            "\n[bright_cyan]4) Afficher la liste des contrats[/bright_cyan]\n"
+            "\n[bright_cyan]5) Afficher la liste des évènements (non associé)[/bright_cyan]\n"
+            "\n[bright_cyan]6) Afficher tous les clients (Lecture seule)[/bright_cyan]\n"  
+            "\n[bright_cyan]7) Afficher tous les contrats (Lecture seule)[/bright_cyan]\n"
+            "\n[bright_cyan]8) Afficher tous les évènements (Lecture seule)[/bright_cyan]\n"
+            "\n[bright_cyan]9) Quitter l'application [/bright_cyan]\n",
+            title="[deep_sky_blue1] Epic Events Menu Gestion[/deep_sky_blue1]",  # Titre du cadre (optionnel)
             expand=True,  # Le cadre prend toute la largeur du terminal
-            border_style="green",  # Style de la bordure
+            border_style="spring_green1",  # Style de la bordure
         )
         console.print(cadre, justify="left")
 
     def display_menu_commercial_view(self):
         cadre = Panel(
-            "\n[bold green]1) Enregistrer un nouveau client[/bold green]\n"
-            "\n[bold green]2) Afficher ma liste de clients[/bold green]\n"  
-            "\n[bold green]3) Afficher les contrats signé[/bold green]\n"  
-            "\n[bold green]4) Afficher les contrats non signé[/bold green]\n"
-            "\n[bold green]5) Afficher les contrats non réglé[/bold green]\n"
-            "\n[bold green]6) Afficher tous les clients (Lecture seule)[/bold green]\n"
-            "\n[bold green]7) Afficher tous les contrats (Lecture seule)[/bold green]\n"
-            "\n[bold green]8) Afficher tous les évènements (Lecture seule)[/bold green]\n"
-            "\n[bold green]9) Quitter l'application [/bold green]\n",
-            title="[blue] Epic Events Menu Commercial[/blue]",  # Titre du cadre (optionnel)
+            "\n[bright_cyan]1) Enregistrer un nouveau client[/bright_cyan]\n"
+            "\n[bright_cyan]2) Afficher ma liste de clients[/bright_cyan]\n"  
+            "\n[bright_cyan]3) Afficher les contrats signé[/bright_cyan]\n"  
+            "\n[bright_cyan]4) Afficher les contrats non signé[/bright_cyan]\n"
+            "\n[bright_cyan]5) Afficher les contrats non réglé[/bright_cyan]\n"
+            "\n[bright_cyan]6) Afficher tous les clients (Lecture seule)[/bright_cyan]\n"
+            "\n[bright_cyan]7) Afficher tous les contrats (Lecture seule)[/bright_cyan]\n"
+            "\n[bright_cyan]8) Afficher tous les évènements (Lecture seule)[/bright_cyan]\n"
+            "\n[bright_cyan]9) Quitter l'application [/bright_cyan]\n",
+            title="[deep_sky_blue1] Epic Events Menu Commercial[/deep_sky_blue1]",  # Titre du cadre (optionnel)
             expand=True,  # Le cadre prend toute la largeur du terminal
-            border_style="green",  # Style de la bordure
+            border_style="spring_green1",  # Style de la bordure
         )
         console.print(cadre, justify="left")
 
@@ -47,7 +47,7 @@ class MenuView:
         console.print(text)
 
     def get_user_input_view(self):
-        user_input = Prompt.ask("[bold green]Faite votre choix [/bold green]")
+        user_input = Prompt.ask("[bright_cyan]Faite votre choix [/bright_cyan]")
         return int(user_input)
 
     def clear_terminal_view(self):
@@ -55,9 +55,9 @@ class MenuView:
 
     def display_message_create_new_collaborator(self):
         cadre = Panel(
-            "\n[bold green]Veuillez saisir les informations du nouveau collaborateur.[/bold green]\n",
-            title="[blue] Epic Events Enregistrement[/blue]",
+            "\n[bright_cyan]Veuillez saisir les informations du nouveau collaborateur.[/bright_cyan]\n",
+            title="[deep_sky_blue1] Epic Events Enregistrement[/deep_sky_blue1]",
             expand=True,
-            border_style="green",
+            border_style="spring_green1",
         )
         console.print(cadre, justify="left")
