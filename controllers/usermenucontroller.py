d5e6c68c2e6956d9cb616513d147ea6868a20d49
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-07 10:06:10 +0100

    Ajout de 2 instances d'attribut suite à une réorganisation de certaine fonction. Modification de nom sur une appel de fonction dans assign_correct_menu_controller. Ajout de la fonction register_new_colaborator_controller, fonction qui permet de gérer la création d'un nouveau collaborateur pour cela il appel 2 fonctions de la vue et 1 fonction d'un controller pour la création. Ajout de la fonction show_collaborator_list_controller, fonction qui permet de gérer l'affichage de la liste des collaborateurs, pour cela il fait appel à 2 fonctions de la vue et 1 appel de fonction d'un controller qui gère la récupération de la liste des collaborateurs. Ajout de la fonction ask_user_if_they_want_to_edit_collaborator, fonction qui permet de demander à l'utilisateur s'il souhaite apporter des modifications sur un collaborateur, pour cela il fait appel à une fonction de la vue pour poser la question et récupérer son choix, puis appel une fonction d'un fichier controller afin de contrôler la saisie de l'utilisateur. Ajout de la fonction ask_user_to_select_collaborator_controller, fonction qui permet de demander à l'utilisateur de choisir le collaborateur à modifier, pour cela il fait appel à une fonction de la vu pour lui demander de choisir l'utilisateur à modifier, on appel une fonction d'un fichier controller qui permet de contrôler la saisie de l'utilisateur, appel d'une fonction d'u fichier controller qui récupère la liste des collaborateurs, appel d'une fonction d'un fichier controller qui vérifie que le collaborateur choisis soit bien dans la liste.

--- controllers/usermenucontroller.py
+++ controllers/usermenucontroller.py
@@ -1,13 +1,15 @@
 class UserMenuController:
-    def __init__(self, menuview, tablecontroller, usercontroller):
+    def __init__(self, menuview, userview, tablecontroller, usercontroller, checkuserinputcontroller):
         self.menu_view = menuview
+        self.user_view = userview
         self.table_c = tablecontroller
         self.user_c = usercontroller
+        self.check_user_input_c = checkuserinputcontroller
 
     def assign_correct_menu_controller(self, username_password):
         username = username_password[0]
         password = username_password[1]
-        info_user = self.table_c.get_info_collaborator(username, password)
+        info_user = self.table_c.get_information_for_all_collaborators_controller(username, password)
         for row in info_user:
             if row.role_id == 1:
                 self.get_menu_commercial_controller()
@@ -34,11 +36,15 @@ class UserMenuController:
                 error = True
             else:
                 if user_input == 1:
-                    self.menu_view.clear_terminal_view()
-                    self.menu_view.display_message_create_new_collaborator()
-                    self.user_c.start_create_user_controller(username, password)
+                    self.register_new_collaborator_controller(username, password)
                 elif user_input == 2:
-                    pass
+                    # Afficher la liste des collaborateurs
+                    self.show_collaborator_list_controller(username, password)
+                    # demander à l'utilisateur s'il souhaite apporter des modifications à un collaborateur
+                    response = self.ask_user_if_they_want_to_edit_collaborator()
+                    if response == "Y":
+                        # Demander à l'utilisateur de selectionner le collaborateur à modifier
+                        self.ask_user_to_select_collaborator_controller(username, password)
                 elif user_input == 3:
                     pass
                 elif user_input == 4:
@@ -55,3 +61,43 @@ class UserMenuController:
                     pass
                 else:
                     error = True
+
+    def register_new_collaborator_controller(self, username, password):
+        self.menu_view.clear_terminal_view()
+        self.menu_view.display_message_create_new_collaborator()
+        self.user_c.start_create_user_controller(username, password)
+
+    def show_collaborator_list_controller(self, username, password):
+        self.menu_view.clear_terminal_view()
+        result = self.table_c.get_information_for_all_collaborators_controller(username, password)
+        self.user_view.display_list_collaborator(result)
+
+    def ask_user_if_they_want_to_edit_collaborator(self):
+        while True:
+            user_input = self.user_view.display_message_edit_collaborator_list()
+            response = self.check_user_input_c.check_user_input_yes_no_controller(user_input)
+            if response == "Y":
+                return response
+            elif response == "N":
+                break
+            else:
+                self.user_view.display_error_message_edit_collaborator_list()
+
+    def ask_user_to_select_collaborator_controller(self, username, password):
+        while True:
+            # On demande à l'utilisateur de saisir l'id du collaborateur à modifier
+            user_input = self.user_view.get_collaborator_id_view()
+            # Contrôle que la saisie soit bien un chiffre
+            result = self.check_user_input_c.check_user_input_isdigit(user_input)
+            if result:
+                # Récupération de la liste des collaborateurs
+                collaborator_list = self.table_c.get_information_for_all_collaborators_controller(username, password)
+                # Vérifie que l'id du collaborateur est présent dans la liste
+                collaborator_info = self.check_user_input_c.check_id_in_list_controller(collaborator_list,
+                                                                                        int(user_input))
+                if collaborator_info:
+                    return collaborator_info
+                else:
+                    self.user_view.display_error_message_choice_view()
+            else:
+                self.user_view.display_error_message_choice_view()
