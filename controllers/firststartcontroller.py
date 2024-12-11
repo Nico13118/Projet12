3dde818f1d3a85bbceda33b30babafd67b0ba0ef
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-11 04:04:16 +0100

    Suite à une réorganisation et aux déplacemenets de plusieurs fonctions, plusieurs appels de fonctions ont été modifiés. Déplacement de la fonction check_user_response_controller un fichier prévu pour recevoir d'autre test de saisie utilisateur. Reconfiguration de la fonction getdatabase_name_controller suite au déplacement de la fonction check_user_response_controller.

--- controllers/firststartcontroller.py
+++ controllers/firststartcontroller.py
@@ -1,15 +1,17 @@
 import re
-YES_RESPONSE = ["Y", "YES", "O", "OUI"]
-NO_RESPONSE = ["N", "NO", 'NON']
 
 
 class FirstStartController:
-    def __init__(self, first_start_view, usercontroller, jsoncontroller, databasecontroller, tablecontroller):
-        self.first_start_v = first_start_view
+    def __init__(self, firststartview, userview, errormessagesview, usercontroller, jsoncontroller, databasecontroller,
+                 tablecontroller, checkuserinputcontroller):
+        self.first_start_v = firststartview
+        self.user_view = userview
+        self.error_messages_v = errormessagesview
         self.user_c = usercontroller
         self.json_c = jsoncontroller
         self.database_c = databasecontroller
         self.table_c = tablecontroller
+        self.check_user_input_c = checkuserinputcontroller
 
     def first_start_controller(self):
         self.first_start_v.first_connection_message_view()  # Affichage panneau d'information
@@ -23,44 +25,37 @@ class FirstStartController:
         self.first_start_v.display_message_create_user_gestion_view()  # Panneau d'information création user Gestion
         self.user_c.start_create_user_controller(username, password)  # Création de l'utilisateur
         self.first_start_v.display_message_end_of_setting_view()
+        self.user_view.prompt_the_user_to_press_the_enter_key()
 
     def username_password_controller(self):
         while True:
-            result_username = self.first_start_v.get_username_view()
-            result_password = self.first_start_v.get_password_view()
-            connexion_test = self.user_c.test_username_password_controller(result_username, result_password)
+            result_username = self.user_view.get_username_view()
+            result_password = self.user_view.get_login_password_view()
+            connexion_test = self.database_c.test_username_password_controller(result_username, result_password)
             if connexion_test:
                 break
             else:
-                self.first_start_v.error_username_password_view()
+                self.error_messages_v.error_username_password_view()
         return result_username, result_password
 
     def get_database_name_controller(self):
         self.first_start_v.display_message_database_view()
         while True:
-            # On demande à l'utilisateur de saisir le nom pour la base de données.
             input_database_name = self.first_start_v.get_database_name_view()
-            # Controle la saisie de l'utilisateur
             x = re.findall(r"[+@=\\/*!^:;,><#-]", input_database_name)
             if x:
-                # Si erreur, afficher un message d'erreur
-                self.first_start_v.display_message_error_database_view(x)
+                self.error_messages_v.display_message_error_database_view(x)
             else:
-                # Sinon, controle et remplacment les espaces dans le nom par des underscores si présent
+                # Sinon, controle et remplacment les espaces dans le nom par des underscores (si présent)
                 database_name = re.sub(r"\s", "_", input_database_name)
-                # Demande à l'utilisateur de confirmer l'utilisation de ce nom pour la base de données
-                user_response = self.check_user_response_controller(database_name)
-                if user_response:
-                    break
-        return database_name
+                while True:
+                    # Demande à l'utilisateur de confirmer l'utilisation de ce nom pour la base de données
+                    user_input = self.first_start_v.get_confirm_created_database_view(database_name)
+                    # Contrôle de la saisie utilisateur.
+                    result = self.check_user_input_c.check_user_input_yes_no_controller(user_input)
+                    if result == "Y":
+                        break
+                    elif result != "Y" or result != "N":
+                        self.error_messages_v.input_error_view()
+                return database_name
 
-    def check_user_response_controller(self, database_name):
-        get_confirm = self.first_start_v.get_confirm_created_database_view(database_name)
-        x = get_confirm.upper()
-        if x in YES_RESPONSE:
-            return True
-        if x in NO_RESPONSE:
-            return False
-        else:
-            self.first_start_v.input_error_view()
-            return False
