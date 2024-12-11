aae72b0b8bae54952be7736611728ea72fcd5e7c
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-11 11:10:25 +0100

    Ajout de la fonction display_message_info_authentication, fonction qui permet d'afficher un panneau d'information afin d'expliquer à l'utilisateur comment s'authentifier. Ajout de la fonction get_username_view, fonction qui permet de récupérer la saisie username.

--- views/userview.py
+++ views/userview.py
@@ -1,10 +1,34 @@
 from rich.prompt import Prompt
 from rich.console import Console
 from rich.table import Table
+from rich.panel import Panel
 console = Console()
 
 
 class UserView:
+    def display_message_info_authentication(self):
+        """
+        Fonction qui permet d'afficher un panneau d'information afin d'expliquer à l'utilisateur
+        comment s'authentifier
+        """
+        cadre = Panel(
+            "\n[bright_cyan]Pour vous connecter, vous devez saisir le nom et prénom de l'utilisateur et mot de passe."
+            "[/bright_cyan]\n"
+            "\n[bright_cyan]Format attendu: dupont_henry[bright_cyan]\n",
+            title="[deep_sky_blue1] Epic Events Connexion[deep_sky_blue1]",  # Titre du cadre (optionnel)
+            expand=True,  # Le cadre prend toute la largeur du terminal
+            border_style="spring_green1",  # Style de la bordure
+        )
+        console.print(cadre, justify="left")
+
+    def get_username_view(self):
+        """
+        Fonction qui permet de récupérer la saisie utilisateur et de la retourner
+        :return: username
+        """
+        username = Prompt.ask("[bright_cyan]Username [bright_cyan]")
+        return username
+
     def get_name_view(self):
         name = Prompt.ask("[bright_cyan]Nom [/bright_cyan]")
         return name
