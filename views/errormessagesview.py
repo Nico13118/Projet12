0d965b4ac5fc46a3d817dd97189c50cf7cb3929e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-11 09:39:46 +0100

    Ajout de la fonction qui permet d'afficher un message d'erreur indiquant à l'utilisateur de saisir que des lettres majuscules et minuscules.

--- views/errormessagesview.py
+++ views/errormessagesview.py
@@ -39,3 +39,12 @@ class ErrorMessagesView:
         text = Text("Le champ ne peut être vide, veuillez recommencer.")
         text.stylize("bold red")
         console.print(text)
+
+    def error_message_field_contains_number(self):
+        """
+        Fonction qui permet d'afficher un message d'erreur indiquant à l'utilisateur de saisir
+        que des lettres majuscules et minuscules
+        """
+        text = Text("Le champ doit contenir que des lettres majuscules et minuscules, veuillez recommencer.")
+        text.stylize("bold red")
+        console.print(text)
