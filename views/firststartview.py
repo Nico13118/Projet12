b161e7b1b6f6116e65b53144ea251ebe7628e9ad
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-11 05:44:58 +0100

    Mise à jour du fichier suite à plusieurs petites modifications sans importance.

--- views/firststartview.py
+++ views/firststartview.py
@@ -1,6 +1,7 @@
 from rich.panel import Panel
 from rich.console import Console
 from rich.prompt import Prompt
+import os
 console = Console()
 
 
@@ -17,32 +18,13 @@ class FirstStartView:
         )
         console.print(cadre, justify="left")
 
-    def get_username_view(self):
-        username = Prompt.ask("[bright_cyan]Username[/bright_cyan]")
-        return username
-
-    def get_password_view(self):
-        mdp = Prompt.ask("[bright_cyan]Password[/bright_cyan]", password=True)
-        return mdp
-
-    def error_username_password_view(self):
-        console.clear()
-        cadre = Panel(
-            "\n[red blink]Erreur !! Identifiant ou mot de passe incorrect...[/red blink]\n\n"
-            "[red]Veuillez recommencer.[/red]",
-            title="[deep_sky_blue1] Epic Events[/deep_sky_blue1]",  # Titre du cadre (optionnel)
-            expand=True,  # Le cadre prend toute la largeur du terminal
-            border_style="spring_green1",  # Style de la bordure
-        )
-        console.print(cadre, justify="left")
-
     def display_message_database_view(self):
-        console.clear()
+        os.system("cls")
         cadre = Panel(
             "\n[red blink]Création de la base de données...[/red blink]\n\n"
             "[bright_cyan]Veuillez saisir le nom de votre base de données que vous souhaitez utiliser.[/bright_cyan]\n"
             "[bright_cyan]Le nom peut être composé de lettres majuscules, minuscules et de chiffres.[/bright_cyan]\n"
-            "[red]Seul le caractère underscore '_' est autorisé comme caractère spécial. Les autres caractères[/red]\n"
+            "[red]Seul le caractère underscore '_' est autorisé comme caractère spécial.\nLes autres caractères[/red]"
             "[red]spéciaux, ainsi que les espaces, sont interdits.[/red]",
             title="[deep_sky_blue1] Epic Events[/deep_sky_blue1]",
             expand=True,
@@ -57,35 +39,16 @@ class FirstStartView:
     def get_confirm_created_database_view(self, database_name):
         user_response = Prompt.ask(
             f"[bright_cyan]Confirmer que vous souhaitez utiliser[/bright_cyan] [red]{database_name!r}[/red]"
-            f"\n[bright_cyan]comme base de données ? ('Y', 'N')[/bright_cyan]")
+            f"[bright_cyan]comme base de données ? ('Y', 'N')[/bright_cyan]")
         return user_response
 
-
-    def display_message_error_database_view(self, x):
-        cadre = Panel(
-            "\n[red blink]Erreur lors de la saisie du nom de la database.[/red blink]\n\n"
-            f"[red]Vous ne pouvez pas utiliser le(s) caractère()s suivant(s): {x}[red]",
-            title="[deep_sky_blue1] Epic Events[/deep_sky_blue1]",
-            expand=True,
-            border_style="spring_green1",
-        )
-        console.print(cadre, justify="left")
-
-    def input_error_view(self):
-        cadre = Panel(
-            "\n[red]Erreur de saisie, veuillez recommencer.[/red ]\n\n",
-            title="[deep_sky_blue1] Epic Events[/deep_sky_blue1]",  # Titre du cadre (optionnel)
-            expand=True,  # Le cadre prend toute la largeur du terminal
-            border_style="spring_green1",  # Style de la bordure
-        )
-        console.print(cadre, justify="left")
-
     def display_message_create_user_gestion_view(self):
-        console.clear()
+        os.system("cls")
         cadre = Panel(
-            "\n[red blink]Création d'un utilisateur MySQL'...[/red blink]\n\n"
-            "[bright_cyan]Veuillez saisir les informations de l'utilisateur qui aura les droits[/bright_cyan]\n"
-            "[bright_cyan]de créer, modifier ou supprimer des utilisateurs dans l'application.[/bright_cyan]\n",
+            "\n[red blink]Création d'un collaborateur Gestion...[/red blink]\n\n"
+            "[bright_cyan]Veuillez saisir les informations du collaborateur qui occupera le poste [/bright_cyan]\n"
+            "[bright_cyan][Gestion] afin qu'il puisse procédéder à la création des autres [/bright_cyan]\n"
+            "[bright_cyan]collaborateurs.[/bright_cyan]\n",
             title="[deep_sky_blue1] Epic Events[/deep_sky_blue1]",
             expand=True,
             border_style="spring_green1",
@@ -93,7 +56,7 @@ class FirstStartView:
         console.print(cadre, justify="left")
 
     def display_message_end_of_setting_view(self):
-        console.clear()
+        os.system("cls")
         cadre = Panel(
             "\n[red blink]Fin du paramétrage de base...[/red blink]\n\n"
             "[bright_cyan]Vous pouvez maintenant vous connecter à l'application.[/bright_cyan]\n",
