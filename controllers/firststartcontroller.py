24c30f9be189c9164c8e5ed4902db8c5493139df
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-13 08:13:39 +0100

    Importation de la classe Session afin d'encapsuler les données de l'utilisateur connecté sous la variable session qui est ajouté en paramètre à plusieurs endroits

--- controllers/firststartcontroller.py
+++ controllers/firststartcontroller.py
@@ -1,4 +1,5 @@
 import re
+from controllers.logincontroller import Session
 
 
 class FirstStartController:
@@ -15,15 +16,13 @@ class FirstStartController:
 
     def first_start_controller(self):
         self.first_start_v.first_connection_message_view()  # Affichage panneau d'information
-        username_password = self.username_password_controller()  # L'utilisateur saisi ses identifiants
-        username = username_password[0]
-        password = username_password[1]
+        session = self.username_password_controller()  # L'utilisateur saisi ses identifiants
         database_name = self.get_database_name_controller()  # L'utilisateur saisi le nom de la bdd.
         self.json_c.create_json_info_file_controller(database_name)  # Création du fichier json
-        self.database_c.create_databases_controller(username, password, database_name)  # Création de la bdd
-        self.table_c.create_table_all_controller(username, password, database_name)  # Création de toutes les tables
+        self.database_c.create_databases_controller(session, database_name)  # Création de la bdd
+        self.table_c.create_table_all_controller(session, database_name)  # Création de toutes les tables
         self.first_start_v.display_message_create_user_gestion_view()  # Panneau d'information création user Gestion
-        self.user_c.start_create_user_controller(username, password)  # Création de l'utilisateur
+        self.user_c.start_create_user_controller(session)  # Création de l'utilisateur
         self.first_start_v.display_message_end_of_setting_view()
         self.user_view.prompt_the_user_to_press_the_enter_key()
 
@@ -31,12 +30,13 @@ class FirstStartController:
         while True:
             result_username = self.user_view.get_username_view()
             result_password = self.user_view.get_login_password_view()
+
             connexion_test = self.database_c.test_username_password_controller(result_username, result_password)
             if connexion_test:
                 break
             else:
                 self.error_messages_v.error_username_password_view()
-        return result_username, result_password
+        return Session(username=result_username, password=result_password, collab_id=None, role_id=None)
 
     def get_database_name_controller(self):
         self.first_start_v.display_message_database_view()
