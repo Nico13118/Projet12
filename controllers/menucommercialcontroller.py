8a431d270d8faead48cd47cfa57ff83fc8b1a516
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-13 13:59:56 +0100

    Configuration de plusieurs attributs d'instance dans la classe MenuCommercialController. Configuration de la fonction get_menu_commercial_controller, fonction qui gère le menu principal du commercial, ajout de l'appel de fonction permettant à l'utilisateur d'enregistrer un nouveau client

--- controllers/menucommercialcontroller.py
+++ controllers/menucommercialcontroller.py
@@ -1,6 +1,45 @@
 class MenuCommercialController:
-    def __init__(self):
-        pass
+    def __init__(self, menuview, userview, errormessagesview, commercialcontroller):
+        self.session = None
+        self.menu_view = menuview
+        self.user_view = userview
+        self.error_messages_v = errormessagesview
+        self.commercial_c = commercialcontroller
 
     def get_menu_commercial_controller(self, session):
-        pass
+        """
+        Fonction qui gère le menu principal du commercial.
+        :param session:
+
+        """
+        error = False
+        while True:
+            self.menu_view.clear_terminal_view()
+            self.menu_view.display_menu_commercial_view()
+            if error:
+                self.error_messages_v.display_error_message_choice_view()
+                error = False
+            user_input = self.user_view.get_user_input_view()
+            if not user_input:
+                error = True
+            else:
+                if user_input == 1:  # Enregistrer un nouveau client.
+                    self.commercial_c.register_new_customer_controller(session)
+                elif user_input == 2:
+                    pass
+                elif user_input == 3:
+                    pass
+                elif user_input == 4:
+                    pass
+                elif user_input == 5:
+                    pass
+                elif user_input == 6:
+                    pass
+                elif user_input == 7:
+                    pass
+                elif user_input == 8:
+                    pass
+                elif user_input == 9:
+                    pass
+                elif user_input == 10:
+                    pass
