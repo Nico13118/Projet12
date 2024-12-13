7f6c14cb59170e6a8cbcb15a79cb49dae89e7e42
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-13 08:16:59 +0100

    configuration de l'attribut d'instance session afin de réucpérer et utiliser une valeur de l'utilisateur connecté.

--- controllers/usermenucontroller.py
+++ controllers/usermenucontroller.py
@@ -1,18 +1,16 @@
 class UserMenuController:
     def __init__(self, menugestioncontroller, menucommercialcontroller, menusupprtcontroller, tablecontroller):
+        self.session = None
         self.menu_gestion_c = menugestioncontroller
         self.menu_commercial_c = menucommercialcontroller
         self.menu_support_c = menusupprtcontroller
         self.table_c = tablecontroller
-
-    def redirect_user_to_his_menu(self, username_password):
-        username = username_password[0]
-        password = username_password[1]
-        info_user = self.table_c.get_information_for_all_collaborators_controller(username, password)
-        for row in info_user:
-            if row.role_id == 1:
-                self.menu_commercial_c.get_menu_commercial_controller(username, password)
-            if row.role_id == 2:
-                self.menu_gestion_c.get_menu_gestion_controller(username, password)
-            if row.role_id == 3:
-                self.menu_support_c.get_menu_support_controller(username, password)
+        
+    def redirect_user_to_his_menu(self, session):
+        self.session = session
+        if self.session.role_id == 1:
+            self.menu_commercial_c.get_menu_commercial_controller(session)
+        elif self.session.role_id == 2:
+            self.menu_gestion_c.get_menu_gestion_controller(session)
+        elif self.session.role_id == 3:
+            self.menu_support_c.get_menu_support_controller(session)
