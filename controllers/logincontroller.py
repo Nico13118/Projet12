ed7d2599b3c2972e7b819747bafa3da3e99b60e5
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-13 08:04:01 +0100

    Petite modification au niveau des noms des attributs d'instances de la classe Session. Modification sur la façon de récupérer les données, l'utilisation de fetchone() remplace la boucle for.

--- controllers/logincontroller.py
+++ controllers/logincontroller.py
@@ -1,8 +1,9 @@
 class Session:
-    def __init__(self, username, password, user_id):
+    def __init__(self, username, password, collab_id, role_id):
         self.username = username
         self.password = password
-        self.user_id = user_id
+        self.collab_id = collab_id
+        self.role_id = role_id
 
 
 class LoginController:
@@ -18,19 +19,19 @@ class LoginController:
         Fonction qui gère la récupération d'un username et password d'un utilisateur et fait appel à une
         fonction pour vérifier que l'utilisateur existe dans la base MySQL.
 
-        :return: username , password
+        :return: session : Correspond à l'utilisateur connecté
         """
-        global info_user_id
         self.user_view.display_message_info_authentication()
         while True:
             username = self.user_view.get_username_view()
             password = self.user_view.get_login_password_view()
-            control_authentication = self.database_c.test_username_password_controller(username, password)
+            session = Session(username=username, password=password, collab_id=None, role_id=None)
+            control_authentication = self.database_c.test_username_password_controller(session)
             if not control_authentication:
                 self.error_messages_v.error_username_password_view()
             else:
                 break
-        result = self.table_c.get_information_for_single_collaborator_controller(username, password)
-        for row in result:
-            info_user_id = row.collab_id
-        return Session(username=username, password=password, user_id=info_user_id)
+        result = self.table_c.get_information_for_single_collaborator_controller(session)
+        select = result.fetchone()
+        session = Session(username=username, password=password, collab_id=select.collab_id, role_id=select.role_id)
+        return session
