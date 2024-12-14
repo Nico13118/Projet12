5d873ac8db2866ff469bff9e0b06327b32b897b1
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-14 17:18:58 +0100

    Ajout de la fonction show_customer_list_controller, fonction qui permet de récupérer la liste des clients qu'elle transmettra à la vue pour y être affiché. Configuration de la fonction show_customer_list_controller, fonction qui demande à l'utilisateur s'il souhaite modifier la fichie d'un client. Configuration de la fonction ask_user_to_select_customer_controller, fonction qui permet de demander à l'utilisateur de saisir le code client à modifier. Quelques modifications suite à un ajout de champ de la table modèle customer.

--- controllers/commercialcontroller.py
+++ controllers/commercialcontroller.py
@@ -1,10 +1,13 @@
 class CommercialController:
-    def __init__(self, menuview, userview, usercontroller, tablecontroller):
+    def __init__(self, menuview, userview, errormessagesview, usercontroller, tablecontroller,
+                 checkuserinputcontroller):
         self.session = None
         self.menu_view = menuview
         self.user_view = userview
         self.user_c = usercontroller
         self.table_c = tablecontroller
+        self.check_user_input_c = checkuserinputcontroller
+        self.error_messages_v = errormessagesview
 
     def register_new_customer_controller(self, session):
         """
@@ -18,7 +21,56 @@ class CommercialController:
         info_name = self.user_c.get_name_controller()
         info_first_name = self.user_c.get_first_name_controller()
         info_email = self.user_c.get_email_controller()
+        info_phone = self.user_c.get_number_phone_controller()
         info_company_name = self.user_c.get_company_name_controller()
 
-        self.table_c.save_customer_in_table_controller(session, info_name, info_first_name, info_email,
+        self.table_c.save_customer_in_table_controller(session, info_name, info_first_name, info_email, info_phone,
                                                        info_company_name)
+
+    def show_customer_list_controller(self, session):
+        """
+        Fonction qui permet de récupérer la liste des clients puis transmet les informations à la vue
+        pour être affichées.
+
+        :param session:
+        """
+        self.menu_view.clear_terminal_view()
+        result = self.table_c.get_list_of_all_customers_controller(session)
+        self.user_view.display_list_customer_view(result)
+
+    def ask_user_if_wants_they_want_to_edit_customer(self):
+        """
+        Fonction qui permet de demander à l'utilisateur s'il souhaite apporter une modification à un utilisateur.
+        La reponse de l'utilisateur est contrôlé.
+
+        :return: Y
+        """
+        while True:
+            user_input = self.user_view.display_message_edit_customer_list_view()
+            response = self.check_user_input_c.check_user_input_yes_no_controller(user_input)
+            if response == "Y":
+                return True
+            elif response == "N":
+                break
+            else:
+                self.error_messages_v.display_error_message_of_values_yes_and_no()
+
+    def ask_user_to_select_customer_controller(self, session):
+        """
+        Fonction qui permet de demander à l'utilisateur de saisir le N° client à modifier.
+        Une recherche est effectuée dans la base avec l'id du client concerné.
+
+        :param session
+        """
+        while True:
+            customer_id = self.user_view.get_customer_code_id_view()
+            result = self.check_user_input_c.check_user_input_isdigit(customer_id)
+            if result:
+                customer_list = self.table_c.get_single_customer_info_with_id_controller(customer_id, session)
+                result_info = [c for c in customer_list if c.custom_id == int(customer_id)]
+                if result_info:
+                    return customer_id
+                else:
+                    self.error_messages_v.display_error_message_choice_view()
+            else:
+                self.error_messages_v.display_error_message_choice_view()
