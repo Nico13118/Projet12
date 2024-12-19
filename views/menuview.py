6ef5e83b0b36258e42c580a83148aebb2a9f6dc9
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-12-19 11:20:01 +0100

    Configuration de la fonction display_edit_menu_of_a_customer_contract_view, fonction qui permet d'afficher les informations d'un contrat avec un sous menu qui permet à l'utilisateur de modifier certain champs du contrat.

--- views/menuview.py
+++ views/menuview.py
@@ -62,6 +62,27 @@ class MenuView:
 
         console.print(table)
 
+    def display_edit_menu_of_a_customer_contract_view(self, result_contract):
+        table = Table(title="[bright_blue]Epic Events\nModifier le contrat d'un client.[/bright_blue]",
+                      style="spring_green1")
+        table.add_column("[bright_blue]Choix[/bright_blue]", justify="center", style="bright_cyan")
+        table.add_column("[bright_blue]Actions[/bright_blue]", justify="left", style="bright_cyan")
+        table.add_column("[bright_blue]Infos contrat[/bright_blue]", justify="left", style="bright_cyan")
+        table.add_column("[bright_blue]Infos supplémentaires[/bright_blue]", justify="left", style="bright_cyan")
+        infos = result_contract.fetchone()
+        table.add_row(" ", " ", " ", f"Nom du client: {infos.custom_name} {infos.custom_first_name}")
+        table.add_row(" ", " ", " ", f"Nom de l'entreprise: {infos.custom_company_name}")
+        table.add_row(" ", " ", " ", f"N° Téléphone: {infos.custom_phone}")
+        table.add_row(" ", " ", " ", f"Commercial: {infos.collab_name} {infos.collab_first_name}")
+        table.add_row(" ", " ", " ", f"Date de création: {infos.contract_created_date}")
+        table.add_row("1", "Modifier la description du contrat", f"{infos.contract_description}")
+        table.add_row("2", "Modifier le prix du contrat", f"{infos.contract_total_price}")
+        table.add_row("3", "Modifier le solde restant", f"{infos.contract_amount_remaining}")
+        table.add_row("4", "Modifier le statut du contrat", f"{infos.contract_status_name}")
+        table.add_row("5", "Quitter", "")
+
+        console.print(table)
+
     def display_menu_commercial_view(self):
         table = Table(title="[deep_sky_blue1] Epic Events - Service Commercial[/deep_sky_blue1]", style="spring_green1")
         table.add_column("[bright_blue]Choix[/bright_blue]", justify="center", style="bright_cyan")
