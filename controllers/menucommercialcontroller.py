f35475508cab89432ee330c617568dbc43ef50d1
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2025-01-13 09:20:12 +0100

    Modification des choix au format str car le format entier peux générer un dysfonctionnement et l'arrêt du programme.

--- controllers/menucommercialcontroller.py
+++ controllers/menucommercialcontroller.py
@@ -25,10 +25,10 @@ class MenuCommercialController:
             if not user_input:
                 error = True
             else:
-                if user_input == 1:  # Enregistrer un client.
+                if user_input == '1':  # Enregistrer un client.
                     self.commercial_c.register_new_customer_controller(session)
 
-                elif user_input == 2:  # Modifier un client
+                elif user_input == '2':  # Modifier un client
                     while True:
                         self.user_c.show_customer_list_controller(session)
                         response = self.commercial_c.ask_user_if_wants_they_want_to_edit_customer()
@@ -38,28 +38,28 @@ class MenuCommercialController:
                         else:
                             break
 
-                elif user_input == 3:  # Afficher tous les contrats (Modification)
+                elif user_input == '3':  # Afficher tous les contrats (Modification)
                     self.menu_view.clear_terminal_view()
                     self.ask_the_user_to_choose_how_to_display_contracts_controller(session)
 
-                elif user_input == 4:  # Créer un événement
+                elif user_input == '4':  # Créer un événement
                     pass
 
-                elif user_input == 5:  # Afficher tous les clients
+                elif user_input == '5':  # Afficher tous les clients
                     list_customer = self.table_c.get_list_of_all_customers_controller(session)
                     self.user_view.display_list_customer_view(list_customer)
                     self.user_view.prompt_the_user_to_press_the_enter_to_return_main_menu()
                     
-                elif user_input == 6:  # Afficher tous les contrats (Lecture seule)
+                elif user_input == '6':  # Afficher tous les contrats (Lecture seule)
                     self.menu_view.clear_terminal_view()
                     result_contract = self.table_c.get_all_contract_controller(session)
                     self.user_view.display_list_contract_view(result_contract, info_title="Liste des contrats")
                     self.user_view.prompt_the_user_to_press_the_enter_to_return_main_menu()
 
-                elif user_input == 7:  # Afficher tous les événements
+                elif user_input == '7':  # Afficher tous les événements
                     pass
 
-                elif user_input == 8:  # Quitter l'application
+                elif user_input == '8':  # Quitter l'application
                     break
                     
                 else:
@@ -81,32 +81,32 @@ class MenuCommercialController:
             self.menu_view.display_edit_menu_of_a_customer_view(result_customer_info)
             result_choice = self.commercial_c.ask_user_which_field_to_edit()
 
-            if result_choice == 1:  # Modifier le nom
+            if result_choice == '1':  # Modifier le nom
                 new_name = self.user_c.get_name_controller()
                 self.table_c.edit_a_field_in_table(session, table_name='customer', field='custom_name',
                                                    new_value=new_name, object_id='custom_id', info_id=customer_id)
 
-            if result_choice == 2:  # Modifier le prénom
+            elif result_choice == '2':  # Modifier le prénom
                 new_first_name = self.user_c.get_first_name_controller()
                 self.table_c.edit_a_field_in_table(session, table_name='customer', field='custom_first_name',
                                                    new_value=new_first_name, object_id='custom_id', info_id=customer_id)
 
-            if result_choice == 3:  # Modifier l'email
+            elif result_choice == '3':  # Modifier l'email
                 new_email = self.user_c.get_email_controller()
                 self.table_c.edit_a_field_in_table(session, table_name='customer', field='custom_email',
                                                    new_value=new_email, object_id='custom_id', info_id=customer_id)
 
-            if result_choice == 4:  # Modifier N° de télélphone
+            elif result_choice == '4':  # Modifier N° de télélphone
                 new_phone = self.user_c.get_number_phone_controller()
                 self.table_c.edit_a_field_in_table(session, table_name='customer', field='custom_phone',
                                                    new_value=new_phone, object_id='custom_id', info_id=customer_id)
 
-            if result_choice == 5:  # Modifier intitulé de l'entreprise
+            elif result_choice == '5':  # Modifier intitulé de l'entreprise
                 new_company_name = self.user_c.get_company_name_controller()
                 self.table_c.edit_a_field_in_table(session, table_name='customer', field='custom_company_name',
                                                    new_value=new_company_name, object_id='custom_id',
                                                    info_id=customer_id)
-            if result_choice == 6:  #
+            elif result_choice == '6':  #
                 break
 
     def ask_the_user_to_choose_how_to_display_contracts_controller(self, session):
@@ -126,7 +126,7 @@ class MenuCommercialController:
                 error = False
             user_input = self.user_view.get_user_input_view()
 
-            if user_input == 1:  # Afficher tous les contrats
+            if user_input == '1':  # Afficher tous les contrats
                 self.menu_view.clear_terminal_view()
                 result_contract = self.table_c.get_all_contract_controller(session)
                 self.user_view.display_list_contract_view(result_contract, info_title="Liste des contrats")
@@ -138,7 +138,7 @@ class MenuCommercialController:
                 else:
                     break
 
-            elif user_input == 2:  # Afficher seulement les contrats non signés
+            elif user_input == '2':  # Afficher seulement les contrats non signés
                 self.menu_view.clear_terminal_view()
                 result_contract = self.table_c.get_all_contract_controller(session)
                 new_list_contract = self.commercial_c.get_unsigned_contracts(result_contract)
@@ -154,7 +154,7 @@ class MenuCommercialController:
                 else:
                     error = True
 
-            elif user_input == 3:  # Afficher seulement les contrats non soldés
+            elif user_input == '3':  # Afficher seulement les contrats non soldés
                 self.menu_view.clear_terminal_view()
                 result_contract = self.table_c.get_all_contract_controller(session)
                 new_list_contract = self.commercial_c.get_unpaid_contracts(result_contract)
@@ -170,7 +170,7 @@ class MenuCommercialController:
                 else:
                     error = True
 
-            elif user_input == 4:  # Retourner au menu principal
+            elif user_input == '4':  # Retourner au menu principal
                 break
 
             else:
