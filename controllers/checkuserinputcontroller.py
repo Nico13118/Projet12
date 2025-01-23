413683775b7185f47396f67106e78e6ac7f37aa9
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2025-01-23 17:14:57 +0100

    Mise à jour du fichier suite à la suppression de plusieurs fonctions non utilisées.

--- controllers/checkuserinputcontroller.py
+++ controllers/checkuserinputcontroller.py
@@ -22,31 +22,6 @@ class CheckUserInputController:
         elif x in NO_RESPONSE:
             return "N"
 
-    def check_id_in_list_controller(self, info_list, user_input, session):
-        """
-        Fonction qui vérifie si l'id choisi est présent dans la liste.
-        S'il y a une correspondance, l'information est retourné.
-
-        :param info_list:
-        :param user_input:
-        :param session
-        :return: collaborator_info
-        """
-        self.session = session
-        result_info = None
-        if self.session.role_id == 1:  # Commercial
-            result_info = [c for c in info_list if c.custom_id == user_input]
-            if not result_info:
-                result_info = False
-
-        elif self.session.role_id == 2:  # Gestion
-            result_info = [c for c in info_list if c.collab_id == user_input]
-            if not result_info:
-                result_info = False
-
-        return result_info
-
-
     def check_user_input_isdigit(self, user_input):
         """
         Fonction qui contrôle que la saisie de l'utilisateur soit bien un chiffre.
@@ -66,10 +41,6 @@ class CheckUserInputController:
         user_input2 = re.sub(r"\s", "", user_input1)
         x = user_input2.isalpha()
         return x
-
-    def check_user_input_of_edit_collaborator_information_menu(self, result_choice):
-        if 1 <= result_choice <= 5:
-            return True
 
     def check_user_input_role_controller(self, role):
         role = role.upper()
