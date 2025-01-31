dc896775889335811a622d3308218da42bdba8c9
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2025-01-31 14:38:38 +0100

    Configuration de l'option 4 du menu permettant à l'utilisateur support d'afficher la listes des événements en lecture seule.

--- controllers/menusupportcontroller.py
+++ controllers/menusupportcontroller.py
@@ -1,8 +1,10 @@
 class MenuSupportController:
-    def __init__(self, menuview, userview, errormessagesview):
+    def __init__(self, menuview, userview, errormessagesview, usercontroller, tablecontroller):
         self.menu_view = menuview
         self.user_view = userview
         self.error_messages_v = errormessagesview
+        self.user_c = usercontroller
+        self.table_c = tablecontroller
 
     def get_menu_support_controller(self, session):
         """
@@ -17,6 +19,8 @@ class MenuSupportController:
             if error:
                 if error == 'error_1':
                     self.error_messages_v.display_error_message_choice_view()
+                elif error == 'error_4':
+                    self.error_messages_v.no_events_to_display_view()
                 error = ''
 
             user_input_choice_menu = self.user_view.get_user_input_view()
@@ -31,7 +35,15 @@ class MenuSupportController:
                 pass
 
             elif user_input_choice_menu == '4':  # Afficher tous les événements (Lecture seule)
-                pass
+                self.menu_view.clear_terminal_view()
+                result_event = self.user_c.get_all_event_controller(session)
+                if result_event:
+                    list_collab_supp = self.table_c.get_information_for_all_collaborators_controller(session)
+                    self.user_view.display_list_all_events_view(result_event, list_collab_supp,
+                                                                info_title="Liste d'événements")
+                    self.user_view.prompt_the_user_to_press_the_enter_to_return_main_menu()
+                else:
+                    error = 'error_4'
 
             elif user_input_choice_menu == '5':  # Quitter l'application
                 break
