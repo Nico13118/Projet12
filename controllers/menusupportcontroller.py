8e7800215fc90f086ff6948aefa33cf4131bcb54
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2025-02-11 15:16:28 +0100

    Configuration de l'option 1 permettant à l'utilisateur de modifier un événement.

--- controllers/menusupportcontroller.py
+++ controllers/menusupportcontroller.py
@@ -18,19 +18,34 @@ class MenuSupportController:
             self.menu_view.display_menu_support_view()
             if error:
                 if error == 'error_1':
-                    self.error_messages_v.display_error_message_choice_view()
+                    self.error_messages_v.no_events_to_display_view()
                 elif error == 'error_2':
                     self.error_messages_v.no_customer_to_display_view()
                 elif error == 'error_3':
                     self.error_messages_v.no_contract_to_display_view()
                 elif error == 'error_4':
                     self.error_messages_v.no_events_to_display_view()
+                elif error == 'error_6':
+                    self.error_messages_v.display_error_message_choice_view()
                 error = ''
 
             user_input_choice_menu = self.user_view.get_user_input_view()
 
             if user_input_choice_menu == '1':  # Modifier un événement
-                pass
+                self.menu_view.clear_terminal_view()
+                # Récupération de la liste des événements qui concerne le callaborateur connecté.
+                result_event1 = self.user_c.get_user_event_list_support_controller(session)
+                if result_event1:
+                    self.user_view.display_collaborator_event_list(result_event1, info_title="Liste d'événements")
+                    #  On affiche la question s'il souhaite modifier un événement
+                    result_response_y_n = self.user_c.ask_user_if_they_want_to_edit_event()
+                    if result_response_y_n:
+                        #  On vérifie que l'id de l'événement saisi par l'utilisateur correspond à celle de la liste
+                        user_input_event_id = self.user_c.ask_user_to_select_event_id_controller(result_event1)
+                        self.user_c.edit_info_event_contract_controller(session, user_input_event_id)
+
+                else:
+                    error = "error_1"
 
             elif user_input_choice_menu == '2':  # Afficher tous les clients (Lecture seule)
                 self.menu_view.clear_terminal_view()
@@ -52,10 +67,10 @@ class MenuSupportController:
 
             elif user_input_choice_menu == '4':  # Afficher tous les événements (Lecture seule)
                 self.menu_view.clear_terminal_view()
-                result_event = self.user_c.get_all_event_controller(session)
-                if result_event:
+                all_event_result = self.user_c.get_all_event_controller(session)
+                if all_event_result:
                     list_collab_supp = self.table_c.get_information_for_all_collaborators_controller(session)
-                    self.user_view.display_list_all_events_view(result_event, list_collab_supp,
+                    self.user_view.display_list_all_events_view(all_event_result, list_collab_supp,
                                                                 info_title="Liste d'événements")
                     self.user_view.prompt_the_user_to_press_the_enter_to_return_main_menu()
                 else:
@@ -65,4 +80,4 @@ class MenuSupportController:
                 break
 
             else:
-                error = 'error_1'
+                error = 'error_6'
