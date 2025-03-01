ce2c1ce6e2eb6d2fa3422a294e6d774b82464bad
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2025-03-01 17:32:06 +0100

    Ajout et configuration de sentry afin de procéder à la gestion des erreurs.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from controllers.maincontroller import MainController
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://129a67e28dfdeecf7ea35deba6b5504c@o4508902464880640.ingest.de.sentry.io/4508902475169872",
+    traces_sample_rate=1.0,
+)
 
 
 def main():
