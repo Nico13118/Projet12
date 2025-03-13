5b411be95b371b38208ebfe6c1d679a2a2edb599
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2025-03-13 09:56:17 +0100

    Amélioration du fichier JsonModel qui permet l'ajout de info_user et secret_key

--- models/jsonmodel.py
+++ models/jsonmodel.py
@@ -1,12 +1,16 @@
 class JsonModel:
-    def __init__(self, database_name, created_date):
+    def __init__(self, database_name, created_date, info_user, secret_key):
         self.database_name = database_name
         self.created_date = created_date
+        self.info_user = info_user
+        self.secret_key = secret_key
 
     def create_json_model(self):
         info_database = {
             "name": self.database_name,
-            "created_date": self.created_date
+            "created_date": self.created_date,
+            "info_user": self.info_user,
+            "secret_key": self.secret_key
         }
         return info_database
 
